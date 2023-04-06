a23de77f0a6d88c826b7ad1fb6c79c6a9193c89c
Fien Yde <fien.yde@student.howest.be>
2023-04-06 20:11:42 +0200

    made servcie with the rules and constructor

---
+++ app/Modules/Monuments/Services/MonumentService.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Modules\Movies\Services;
+
+
+class MonumentService extends Services
+{
+    protected $_rules = [
+        "name" => "required|string|max:50",
+        "description" => "required|text",
+        "location" => "required|json",
+        "historicalSignificance" => "nullable|text";
+        "type" => "required|enum";
+        "yearOfContstruction" => "required|year";
+        "monumentDesigner" => "required|string";
+        "accessibility" => "required|json";
+        "usedMaterials" => "nullable|json";
+        "dimensions" => "nullable|json";
+        "weight" => "nullable|integer";
+        "costToConstruct" => "nullable|integer";
+        "images" => "required|json";
+        "audiovisualSources" => "nullable|json" ];
+
+        public function __construct(Monument $model) {
+            parent::__construct($model);
+        }
+}
