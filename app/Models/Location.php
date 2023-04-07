5714683cce4c41c998683064304ffebbccb8ff14
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-07 18:33:02 +0200

    add relations and fields to location model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'latitude',
+        'longitude',
+        'street',
+        'number',
+        'city',
+    ];
+
+    public function monument()
+    {
+        return $this->hasOne(Monument::class);
+    }
 }
