206b6939b20a58b83d3954a64b2ac8d8b63bb163
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-07 18:33:23 +0200

    add relations to monument model

--- app/Models/Monument.php
+++ app/Models/Monument.php
@@ -9,20 +9,41 @@ class Monument extends Model
 {
     use HasFactory;
 
-    protected $filleable = [
-        'id', 
-        'description', 
-        'location',
-        'historical_significance', 
-        'type', 
-        'year_of_construction', 
-        'monument_designer', 
-        'accessibility', 
-        'used_materials', 
-        'dimensions', 
-        'weight', 
-        'cost_to_construct', 
-        'images', 
-        'audiovisual_sources'
+    protected $fillable = [
+        'name',
+        'description',
+        'historical_significance',
+        'type',
+        'year_of_construction',
+        'monument_designer',
+        'accessibility',
+        'used_materials',
+        'weight',
+        'cost_to_construct',
+        'language',
     ];
+
+    protected $casts = [
+        'used_materials' => 'array',
+    ];
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function dimension()
+    {
+        return $this->belongsTo(Dimension::class);
+    }
+
+    public function images()
+    {
+        return $this->belongsToMany(Image::class);
+    }
+
+    public function audiovisualSources()
+    {
+        return $this->belongsToMany(AudiovisualSource::class);
+    }
 }
