7c1d425ce5124d45556ffafe516387a7a7dfe357
vagrant <vagrant@homestead>
2023-04-09 14:26:31 +0000

    use service in the monument seeder to optimize code

--- database/seeders/MonumentSeeder.php
+++ database/seeders/MonumentSeeder.php
@@ -9,72 +9,74 @@ use App\Models\Location;
 use App\Models\Dimension;
 use App\Models\Image;
 use App\Models\AudiovisualSource;
+use App\Modules\Monuments\Services\MonumentService;
+use Illuminate\Support\Facades\DB;
 
 class MonumentSeeder extends Seeder
 {
+    private $_service;
+    public function __construct(MonumentService $service){
+        $this->_service = $service;
+    }
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         $file = fopen(storage_path('app/data/csv/monuments.csv'), 'r');
-
         $header = fgetcsv($file);
+        $monuments = $this->parseMonuments($file, $header);
+        fclose($file);
+    
+        $this->_service->addMultipleMonuments($monuments);
+    }
+
+    protected function parseMonuments($file, $header)
+    {
+        $monuments = [];
 
         while (($data = fgetcsv($file)) !== false) {
             $data = array_combine($header, $data);
 
-            $usedMaterials = explode(',', $data['used_materials']);
-
-            $location = Location::create([
-                'latitude' => $data['location_latitude'],
-                'longitude' => $data['location_longitude'],
-                'street' => $data['location_street'] ?: null,
-                'number' => $data['location_number'] ?: null,
-                'city' => $data['location_city'],
-            ]);
-
-            $dimension = Dimension::create([
-                'height' => $data['dimensions_height'],
-                'width' => $data['dimensions_width'],
-                'depth' => $data['dimensions_depth'],
-            ]);
-
-            $audiovisual = AudiovisualSource::create([
-                'title' => $data['audiovisual_source_title'],
-                'url' => $data['audiovisual_source_url'],
-                'type' => $data['audiovisual_source_type'],
-            ]);
-
-            $monument = Monument::create([
+            $monumentData = [
                 'name' => $data['name'],
                 'description' => $data['description'],
-                'location_id' => $location->id,
+                'location' => [
+                    'latitude' => $data['latitude'],
+                    'longitude' => $data['longitude'],
+                    'street' => $data['street'] ?: null,
+                    'number' => $data['number'] ?: null,
+                    'city' => $data['city'],
+                ],
                 'historical_significance' => $data['historical_significance'] ?: null,
                 'type' => $data['type'],
                 'year_of_construction' => $data['year_of_construction'],
                 'monument_designer' => $data['monument_designer'],
                 'accessibility' => $data['accessibility'],
-                'used_materials' => $usedMaterials,
-                'dimensions_id' => $dimension->id,
+                'used_materials' => explode(',', $data['used_materials']),
+                'dimensions' => [
+                    'height' => $data['height'],
+                    'width' => $data['width'],
+                    'depth' => $data['depth'],
+                ],
                 'weight' => $data['weight'] ?: null,
                 'cost_to_construct' => $data['cost_to_construct'] ?: null,
-                'audiovisual_source_id' => $audiovisual->id,
+                'images' => [
+                    'urls' => $data['images_urls'],
+                    'captions' => $data['images_captions'],
+                ],
+                'audiovisual_source' => [
+                    'title' => $data['audiovisual_source_title'],
+                    'url' => $data['audiovisual_source_url'],
+                    'type' => $data['audiovisual_source_type'],
+                ],
                 'language' => $data['language'],
-            ]);
+            ];
 
-            $images = json_decode($data['images_url'], true);
-            $captions = json_decode($data['images_caption'], true);
-            
-            foreach ($images as $key => $image) {
-                $img = Image::create([
-                    'monument_id' => $monument->id,
-                    'url' => $image,
-                    'caption' => $captions[$key],
-                ]);
-            }
+            $monuments[] = $monumentData;
         }
 
-        fclose($file);
+        return $monuments;
     }
 }
