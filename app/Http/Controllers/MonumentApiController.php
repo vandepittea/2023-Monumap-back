e8ad3531d0bfea2aa5629d395e91d0b4919dbe62
Fien Yde <fien.yde@student.howest.be>
2023-04-09 20:34:46 +0200

    added monuments to csv file

--- app/Http/Controllers/MonumentApiController.php
+++ app/Http/Controllers/MonumentApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Modules\Movies\Services\MonumentService;
+use App\Modules\Monuments\Services\MonumentService;
 
 
 class MonumentApiController extends Controller
