d32a322e01be3c3d11aa12d0ccccdd88843ea275
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-21 21:10:39 +0200

    add seeder

--- database/seeders/MonumentSeeder.php
+++ database/seeders/MonumentSeeder.php
@@ -3,63 +3,52 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Modules\Monuments\Services\MonumentService;
-use Illuminate\Support\Facades\Log;
+use App\Models\Monument;
+use App\Models\Location;
+use App\Models\Dimensions;
+use App\Models\Image;
+use App\Models\AudiovisualSource;
+use App\Models\MonumentLanguage;
 
 class MonumentSeeder extends Seeder
 {
-    private $_service;
-    public function __construct(MonumentService $service){
-        $this->_service = $service;
-    }
-
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $file = fopen(storage_path('app/data/csv/monuments.csv'), 'r');
         $header = fgetcsv($file);
-        $monuments = $this->parseMonuments($file, $header);
-        $jsonMonuments = json_encode($monuments, JSON_PRETTY_PRINT);
-        Log::info($jsonMonuments);
-        fclose($file);
-    
-        $this->_service->addMultipleMonuments($monuments);
-    }
-
-    protected function parseMonuments($file, $header)
-    {
-        $monuments = [];
 
         while (($data = fgetcsv($file)) !== false) {
             $data = array_combine($header, $data);
 
-            $monumentData = [
-                'year_of_construction' => (int) $data['year_of_construction'],
-                'monument_designer' => $data['monument_designer'],
-                'weight' => (int) $data['weight'],
-                'cost_to_construct' => (float) $data['cost_to_construct'],
-                'location' => [
-                    'latitude' => (float) $data['latitude'],
-                    'longitude' => (float) $data['longitude'],
-                    'street' => $data['street'],
-                    'number' => (int) $data['number'],
-                    'city' => $data['city'],
-                ],
-                'dimensions' => $this->parseDimensions($data),
-                'images' => $this->parseImages($data),
-                'audiovisual_source' => $this->parseAudiovisualSource($data),
-                'monument_language' => $this->parseMonumentLanguage($data),
-            ];
+            $location = $this->createLocation($data);
+            $dimensions = $this->parseDimensions($data);
+            $audiovisualSource = $this->parseAudiovisualSource($data);
 
-            $monuments[] = $monumentData;
+            $monument = $this->createMonument($data, $location, $dimensions, $audiovisualSource);
+
+            $this->parseImages($data, $monument);
+
+            $monumentLanguages = $this->parseMonumentLanguage($data);
+            $this->saveMonumentLanguages($monumentLanguages, $monument);
         }
 
-        return $monuments;
+        fclose($file);
     }
 
-    protected function parseDimensions($data)
+    private function createLocation($data)
+    {
+        $locationData = [
+            'latitude' => (float) $data['latitude'],
+            'longitude' => (float) $data['longitude'],
+            'street' => $data['street'],
+            'number' => (int) $data['number'],
+            'city' => $data['city']
+        ];
+
+        return Location::firstOrCreate($locationData);
+    }
+
+    private function parseDimensions($data)
     {
         $height = (float) $data['height'];
         $width = (float) $data['width'];
@@ -69,90 +58,111 @@ class MonumentSeeder extends Seeder
             return null;
         }
 
-        return (object) [
+        $dimensionsData = [
             'height' => $height,
             'width' => $width,
             'depth' => $depth,
         ];
+
+        return Dimensions::firstOrCreate($dimensionsData);
     }
 
-    protected function parseImages($data)
-    {
-        $images = [];
-
-        $imageUrlKey = "images_url";
-        $captionDutchKey = "captions_dutch";
-        $captionEnglishKey = "captions_english";
-
-        $imageUrls = explode(',', $data[$imageUrlKey]);
-        $captionsDutch = explode(',', $data[$captionDutchKey]);
-        $captionsEnglish = explode(',', $data[$captionEnglishKey]);
-
-        foreach ($imageUrls as $index => $imageUrl) {
-            $imageUrl = trim($imageUrl);
-            $captionDutch = trim($captionsDutch[$index] ?? '');
-            $captionEnglish = trim($captionsEnglish[$index] ?? '');
-
-            if (!empty($imageUrl)) {
-                $image = [
-                    'url' => $imageUrl,
-                    'image_language' => [
-                        [
-                            'caption' => $captionDutch,
-                            'language' => 'Dutch',
-                        ],
-                        [
-                            'caption' => $captionEnglish,
-                            'language' => 'English',
-                        ],
-                    ],
-                ];
-
-                $images[] = $image;
-            }
-        }
-
-        return $images;
-    }           
-
-    protected function parseAudiovisualSource($data)
+    private function parseAudiovisualSource($data)
     {
         if (empty($data['audiovisual_source_url']) && empty($data['audiovisual_source_type']) && empty($data['audiovisual_source_language_title_dutch']) && empty($data['audiovisual_source_language_title_english'])) {
             return null;
         }
 
-        return [
+        $audiovisualSourceData = [
             'url' => $data['audiovisual_source_url'],
             'type' => $data['audiovisual_source_type'],
-            'audiovisual_source_language' => [
-                [
-                    'title' => $data['audiovisual_source_language_title_dutch'],
-                    'language' => 'Dutch',
-                ],
-                [
-                    'title' => $data['audiovisual_source_language_title_english'],
-                    'language' => 'English',
-                ],
+        ];
+
+        $audiovisualSource = AudiovisualSource::firstOrCreate($audiovisualSourceData);
+
+        $audiovisualSourceLanguage = [
+            [
+                'title' => $data['audiovisual_source_language_title_dutch'],
+                'language' => 'Dutch'
+            ],
+            [
+                'title' => $data['audiovisual_source_language_title_english'],
+                'language' => 'English'
             ],
         ];
+
+        foreach ($audiovisualSourceLanguage as $language) {
+            $audiovisualSource->audiovisualSourceLanguage()
+                ->updateOrCreate(['title' => $language['title']], $language);
+        }
+
+        return $audiovisualSource;
     }
 
-    protected function parseAccessibility($data, $language)
+    private function createMonument($data, $location, $dimensions, $audiovisualSource)
     {
-        $accessibilityKey = "monument_language_accessibility_{$language}";
-        return explode(',', $data[$accessibilityKey]);
+        $monumentData = [
+            'year_of_construction' => (int) $data['year_of_construction'],
+            'monument_designer' => $data['monument_designer'],
+            'weight' => (int) $data['weight'],
+            'cost_to_construct' => (float) $data['cost_to_construct'],
+            'location_id' => $location->id,
+            'dimensions_id' => $dimensions ? $dimensions->id : null,
+        'audiovisual_source_id' => $audiovisualSource ? $audiovisualSource->id : null,
+        ];
+
+        return Monument::create($monumentData);
     }
 
-    protected function parseUsedMaterials($data, $language)
+    private function parseImages($data, $monument)
     {
-        $usedMaterialsKey = "monument_language_used_materials_{$language}";
-        return explode(',', $data[$usedMaterialsKey]);
+        $images = [];
+    
+        $imageUrlKey = "images_url";
+        $captionDutchKey = "captions_dutch";
+        $captionEnglishKey = "captions_english";
+    
+        $imageUrls = explode(',', $data[$imageUrlKey]);
+        $captionsDutch = explode(',', $data[$captionDutchKey]);
+        $captionsEnglish = explode(',', $data[$captionEnglishKey]);
+    
+        foreach ($imageUrls as $index => $imageUrl) {
+            $imageUrl = trim($imageUrl);
+            $captionDutch = trim($captionsDutch[$index] ?? '');
+            $captionEnglish = trim($captionsEnglish[$index] ?? '');
+    
+            if (!empty($imageUrl)) {
+                $imageData = [
+                    'url' => $imageUrl,
+                    'monument_id' => $monument->id,
+                ];
+    
+                $image = Image::create($imageData);
+    
+                $imageLanguages = [
+                    [
+                        'caption' => $captionDutch,
+                        'language' => 'Dutch',
+                    ],
+                    [
+                        'caption' => $captionEnglish,
+                        'language' => 'English',
+                    ],
+                ];
+
+                $image->imageLanguage()->createMany($imageLanguages);
+    
+                $images[] = $image;
+            }
+        }
+    
+        return $images;
     }
 
-    protected function parseMonumentLanguage($data)
+    private function parseMonumentLanguage($data)
     {
         return [
-            [
+            MonumentLanguage::make([
                 'language' => 'Dutch',
                 'name' => $data['monument_language_name_dutch'],
                 'description' => $data['monument_language_description_dutch'],
@@ -160,8 +170,8 @@ class MonumentSeeder extends Seeder
                 'type' => $data['monument_language_type_dutch'],
                 'accessibility' => $this->parseAccessibility($data, 'dutch'),
                 'used_materials' => $this->parseUsedMaterials($data, 'dutch'),
-            ],
-           [
+            ]),
+            MonumentLanguage::make([
                 'language' => 'English',
                 'name' => $data['monument_language_name_english'],
                 'description' => $data['monument_language_description_english'],
@@ -169,7 +179,27 @@ class MonumentSeeder extends Seeder
                 'type' => $data['monument_language_type_english'],
                 'accessibility' => $this->parseAccessibility($data, 'english'),
                 'used_materials' => $this->parseUsedMaterials($data, 'english'),
-            ],
+            ]),
         ];
+    }
+
+    private function parseAccessibility($data, $language)
+    {
+        $accessibilityKey = "monument_language_accessibility_{$language}";
+        return explode(',', $data[$accessibilityKey]);
+    }
+
+    private function parseUsedMaterials($data, $language)
+    {
+        $usedMaterialsKey = "monument_language_used_materials_{$language}";
+        return explode(',', $data[$usedMaterialsKey]);
+    }
+
+    private function saveMonumentLanguages($monumentLanguages, $monument)
+    {
+        foreach ($monumentLanguages as $monumentLanguage) {
+            $monumentLanguage->monument_id = $monument->id;
+            $monumentLanguage->save();
+        }
     }
 }
